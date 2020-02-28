72ab4a7435b0c85ac2499689d1615705eadd061f
Erica <ericaquan.edu@gmail.com>
2020-02-28 22:09:11 +0000

    create model file to store the data base. add comment test in seeds.js and render out through show.ejs page.

--- app.js
+++ app.js
@@ -2,55 +2,41 @@ var express 	= require("express"),
 	app 		= express(),
 	request 	= require("request"),
 	bodyParser 	= require("body-parser"),
-	mongoose 	= require("mongoose")
+	mongoose 	= require("mongoose"),
+	Campground  = require("./models/campground"),
+	seedDB		= require("./seeds")
 
+seedDB();
 mongoose.connect("mongodb://localhost/trave_world", {useNewUrlParser: true, useUnifiedTopology: true});
 app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine", "ejs");
-
-
-// SCHEMA SETUP
-var  campgroundSchema = new mongoose.Schema({
-	name: String,
-	image: String
-});
-
-var Campground = mongoose.model("Campground", campgroundSchema);
-
-// Campground.create(
-// 	{
-// 		name: "Montain Goat's Rest", image: "https://images.unsplash.com/photo-1581888517319-570283943d82?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
-// 	}, function(err, campground){
-// 		if(err){
-// 			console.log(err);
-// 		} else {
-// 			console.log("NEWLY CREATED CAMPGROUND");
-// 			console.log(campground);
-// 		}
-// });
 
 
 app.get("/", function(req, res){
 	res.render("landing")
 })
 
+// INDEX - show all campgrounds.
 app.get("/campgrounds", function(req, res){
 	//Get all campgrounds from DB
 	Campground.find({}, function(err, allCampgrounds){
 		if(err){
 			console.log(err);
 		} else {
-			res.render("campgrounds", {campgrounds:allCampgrounds});
+			res.render("index", {campgrounds:allCampgrounds});
 		}
 	});
 });
 
+//CREATE - add new campground to DB.
 app.post("/campgrounds", function(req, res){
-	var name = req.body.name;
-	var image = req.body.image;
-	var newCampground = {
+	var name = req.body.name,
+		image = req.body.image,
+		desc = req.body.description,
+		newCampground = {
 		name: name,
-		image: image
+		image: image,
+		description: desc
 	}
 	// Create a new campground and save to DB
 	Campground.create(newCampground, function(err, newlyCreated){
@@ -63,9 +49,23 @@ app.post("/campgrounds", function(req, res){
 	})
 })
 
+// NEW - show form to create new campground.
 app.get("/campgrounds/new", function(req, res){
-	
 	res.render("new.ejs")
+})
+
+//SHOW - shows more info about one campground.
+app.get("/campgrounds/:id", function(req, res){
+	//find the campground with provided ID
+	Campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground){
+		if(err){
+			console.log(err);
+		} else {
+			console.log(foundCampground)
+			//render show template with that campground
+			res.render("show", {campground: foundCampground})
+		}
+	})
 })
 
 app.listen(3000, function(){
---
+++ models/campground.js
@@ -0,0 +1,17 @@
+// SCHEMA SETUP
+var mongoose = require("mongoose");
+
+var  campgroundSchema = new mongoose.Schema({
+	name: String,
+	image: String,
+	description: String,
+	comments: [
+		{
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "Comment"
+		}
+	]
+});
+
+// var Campground = mongoose.model("Campground", campgroundSchema);
+module.exports = mongoose.model("Campground", campgroundSchema);
