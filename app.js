1d4a2cb51d37399a4c1554d00e251c819ad4e871
Erica <ericaquan.edu@gmail.com>
2020-03-05 22:12:57 +0000

    adding user.js in models file and create register route(get form and post route)

--- app.js
+++ app.js
@@ -1,17 +1,32 @@
-var express 	= require("express"),
-	app 		= express(),
-	request 	= require("request"),
-	bodyParser 	= require("body-parser"),
-	mongoose 	= require("mongoose"),
-	Campground  = require("./models/campground"),
-	Comment 	= require("./models/comment"),
-	seedDB		= require("./seeds")
+var express 		= require("express"),
+	app 			= express(),
+	request 		= require("request"),
+	bodyParser 		= require("body-parser"),
+	mongoose 		= require("mongoose"),
+	passport		= require("passport"),
+	LocalStrategy 	= require("passport-local"),
+	Campground  	= require("./models/campground"),
+	Comment 		= require("./models/comment"),
+	User 			= require("./models/user"),
+	seedDB			= require("./seeds")
 
 // seedDB();
 mongoose.connect("mongodb://localhost/trave_world", {useNewUrlParser: true, useUnifiedTopology: true});
 app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
+
+// PASSPORT CONFIGURATION
+app.use(require("express-session")({
+	secret: "this is really secret!",
+	resave: false,
+	saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function(req, res){
 	res.render("landing")
@@ -106,6 +121,31 @@ app.post("/campgrounds/:id/comments", function(req, res){
 		}
 	});
 })
+
+//==============================================
+// AUTH ROUTES
+//==============================================
+
+//show register form
+app.get("/register", function(req, res){
+	res.render("register")
+})
+//handle sign up logic
+app.post("/register", function(req, res){
+	var newUser = new User({username: req.body.username});
+	User.register(newUser, req.body.password, function(err, user){
+		if(err){
+			console.log(err);
+			return res.render("register")
+		}
+		passport.authenticate("local")(req, res, function(){
+			res.redirect("/campgrounds")
+		});
+	});
+});
+
+
+
 
 
 app.listen(3000, function(){
